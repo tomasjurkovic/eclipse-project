fb69edace89e931b60069fb40478adeac29a809c
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-06-21 10:22:03 +0200

    mock API example
    created mock response for API which is currently being developed.

--- FirstProject/src/files/payload.java
+++ FirstProject/src/files/payload.java
@@ -20,5 +20,31 @@ public class payload {
 				+ "  \"language\": \"French-IN\"\r\n"
 				+ "}";
 	}
+	
+	public static String CoursePrice() {
+		return "{\r\n"
+				+ "  \"dashboard\": {\r\n"
+				+ "    \"purchaseAmount\": 910,\r\n"
+				+ "    \"website\": \"rahultshettyacademy.com\"\r\n"
+				+ "  },\r\n"
+				+ "  \"courses\": [\r\n"
+				+ "    {\r\n"
+				+ "      \"title\": \"Selenium Python\",\r\n"
+				+ "      \"price\": 50,\r\n"
+				+ "      \"copies\": 6\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"title\": \"Cypress\",\r\n"
+				+ "      \"price\": 40,\r\n"
+				+ "      \"copies\": 4\r\n"
+				+ "    },\r\n"
+				+ "    {\r\n"
+				+ "      \"title\": \"RPA\",\r\n"
+				+ "      \"price\": 45,\r\n"
+				+ "      \"copies\": 10\r\n"
+				+ "    }\r\n"
+				+ "  ]\r\n"
+				+ "}";
+	}
 
 }
